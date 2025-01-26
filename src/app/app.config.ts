ded46f51f70857501681eb82c05cad9dc16e05f1
emna-dammak <emna.dammak@insat.ucar.tn>
2025-01-26 12:22:56 +0100

    added components styles and auth interceptor

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,18 +4,20 @@ import { provideToastr } from 'ngx-toastr';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
-import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { AuthentificationInterceptorProvider } from './interceptors/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideClientHydration(),
-    provideHttpClient(withFetch()),
+    provideHttpClient(withInterceptorsFromDi()),
     provideToastr({
       positionClass: 'toast-top-right',
       preventDuplicates: true,
       timeOut: 3000,
     }),
+    AuthentificationInterceptorProvider,
   ],
 };
